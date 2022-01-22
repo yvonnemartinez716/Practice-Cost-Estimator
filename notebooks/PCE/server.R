612642fd4f1d3e433ebd0b78c00b4a1e66eef8bb
yvonnemartinez716 <yvonne.e.martinez@gmail.com>
2022-01-21 21:17:41 -0600

    added conditional panel, radio buttons and fixed place value issue

--- notebooks/PCE/server.R
+++ notebooks/PCE/server.R
@@ -15,67 +15,77 @@ library(datasets)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output,session) {
-    values <-reactiveValues(rvu = (head(rvu,1) %>% 
-                              mutate(QTY = 0, Total = FacilityTotal) %>% 
-                              head(0)))
-    #output$Relative <- renderTable({ values$rvu}, striped = TRUE)
-          
-          observeEvent(input$addButton,{
-      rvutable <- subset(rvu, rvu$HCPS ==input$HCPSFilter) %>% 
-        mutate(QTY = input$num + 0, Total = FacilityTotal * input$num)
-        values$rvu <-rbind(values$rvu, rvutable)
-                              })
-          observeEvent(input$remove,{
-                  values$rvu <-filter(values$rvu, HCPS != input$remove_HCPS)
-          })
-          
-          output$removerow <-renderUI({
-            selectInput(inputId = "remove_HCPS", label = "Remove HCPS Code",
-                                choices = (values$rvu$HCPS))
-            
-                      })
-          
-          
-      sketch <- htmltools::withTags(table(
-          tableHeader(Names), tableFooter(FooterName))
-          )
-          
-          opts <- list(
-            dom = 'Bfrtip', buttons = list(list(extend='collection',text='Download',buttons = list('copy','csv','excel','pdf'))),
-            footerCallback = JS(
-              "function( tfoot, data, start, end, display ) {",
-              "var api = this.api(), data;",
-              "$( api.column(6).footer()).html('SubTotal:  '+",
-              "api.column(6).data().reduce( function ( a, b ) {",
-              "return a + b;",
-              "} )",
-              ");",
-              "$( api.column(7).footer()).html('QTY Total: '+",
-              "api.column(7).data().reduce( function ( a, b ) {",
-              "return a + b;",
-              "} )",
-              ");",
-              "$( api.column(8).footer()).html('Total: '+",
-              "api.column(8).data().reduce( function ( a, b ) {",
-              "return a + b;",
-              "} )",
-              ");","}")
-          )
-          
-          output$display <- DT::renderDataTable(container = sketch,extensions = 'Buttons',options = opts,{
-          data.frame (values$rvu)
-         
-          })
-          output$plot <- renderPlot({
-            values$rvu %>%
-              ggplot(aes(x=HCPS, y = Total)) +
-              geom_col()
-})        
-          
-          
-          
-    })
-        
-
-
+  values <-reactiveValues(rvu = (head(rvu,1) %>% 
+                                   mutate(QTY = 0, Total = FacilityTotal) %>% 
+                                   head(0)))
+  #output$Relative <- renderTable({ values$rvu}, striped = TRUE)
   
+  observeEvent(input$addButton,{
+    rvutable <- subset(rvu, rvu$HCPS ==input$HCPSFilter) %>% 
+      mutate(QTY = input$num + 0, Total = FacilityTotal * input$num)
+    values$rvu <-rbind(values$rvu, rvutable)
+  })
+  observeEvent(input$descButton,{
+    rvutable <- subset(rvu, rvu$HCPS ==input$HCPSdesc) %>% 
+    mutate(QTY = input$numdesc + 0, Total = FacilityTotal * input$numdesc)
+    values$rvu <-rbind(values$rvu, rvutable)
+  })
+  
+  observeEvent(input$remove,{
+    values$rvu <-filter(values$rvu, HCPS != input$remove_HCPS)
+  })
+  output$SelectHCPScode <- renderUI({
+    selectizeInput("HCPSdesc", label = h3("Select HCPS Code"), choices = subset(rvu, rvu$Description ==input$descFilter)$HCPS)
+    
+    
+  })
+  output$removerow <-renderUI({
+    selectInput(inputId = "remove_HCPS", label = "Remove HCPS Code",
+                choices = (values$rvu$HCPS))
+    
+  })
+  
+  
+  sketch <- htmltools::withTags(table(
+    tableHeader(Names), tableFooter(FooterName))
+  )
+  
+  opts <- list(
+    dom = 'Bfrtip', buttons = list(list(extend='collection',text='Download',buttons = list('copy','csv','excel','pdf'))),
+    footerCallback = JS(
+      "function( tfoot, data, start, end, display ) {",
+      "var api = this.api(), data;",
+      "$( api.column(6).footer()).html('SubTotal:  '+",
+      "api.column(6).data().reduce( function ( a, b ) {",
+      "return (a + b).toFixed(2);",
+      "} )",
+      ");",
+      "$( api.column(7).footer()).html('QTY Total: '+",
+      "api.column(7).data().reduce( function ( a, b ) {",
+      "return a + b;",
+      "} )",
+      ");",
+      "$( api.column(8).footer()).html('Total: '+",
+      "api.column(8).data().reduce( function ( a, b ) {",
+      "return (a + b).toFixed(2);",
+      "} )",
+      ");","}")
+  )
+  
+  output$display <- DT::renderDataTable(container = sketch,extensions = 'Buttons',options = opts,{
+    data.frame (values$rvu)
+    
+  })
+  output$plot <- renderPlot({
+    values$rvu %>%
+      ggplot(aes(x=HCPS, y = Total)) +
+      geom_col()
+  })        
+  
+  
+  
+})
+
+
+
+
--- notebooks/PCE/ui.R
+++ notebooks/PCE/ui.R
@@ -13,24 +13,36 @@ library(shinydashboard)
 library(ggplot2)
 
 fluidPage(theme = shinytheme("cerulean"),
-  titlePanel("Practice Cost Estimator"),
-  sidebarLayout(
-    sidebarPanel(width = 2,
-      selectizeInput("HCPSFilter", label = h3("Select HCPS Code"), choices = rvu$HCPS),
-      numericInput("num", label = h3("Quantity"), value = 1),
-      actionButton("addButton", "Add", icon("Add")),
-      uiOutput("removerow"),
-       
-      actionButton(inputId = "remove", label = "Remove HCPS Code")
-      ),
-    mainPanel(
-      #tableOutput("Relative"), 
-      dataTableOutput("display"),
-      plotOutput("plot")
-      
-    )
-  )
+          titlePanel("Practice Cost Estimator"),
+          sidebarLayout(
+            sidebarPanel(width = 2,
+                         radioButtons("radio", label = h3(" "),
+                                      choices = list("Select by HCPS Code" = 1, "Select by Description" = 2), 
+                                      selected = 1),
+                         conditionalPanel("input.radio == 1", selectizeInput("HCPSFilter", label = h3("Select HCPS Code"), choices = rvu$HCPS),
+                           numericInput("num", label = h3("Quantity"), value = 1),
+                           actionButton("addButton", "Add", icon("Add"))
+                         ),
+                         conditionalPanel("input.radio == 2",selectizeInput("descFilter", label = h3("Select Description"), choices = rvu$Description),
+                                          uiOutput("SelectHCPScode"),
+                                          numericInput("numdesc", label = h3("Quantity"), value = 1),
+                                          actionButton("descButton", "Add", icon("Add"))
+                                          
+                         ),
+                         
+                         
+                                             uiOutput("removerow"),
+                         
+                         actionButton(inputId = "remove", label = "Remove HCPS Code")
+            ),
+            mainPanel(
+              #tableOutput("Relative"), 
+              dataTableOutput("display"),
+              plotOutput("plot")
+              
+            )
+          )
 )
 
 
-  
+
